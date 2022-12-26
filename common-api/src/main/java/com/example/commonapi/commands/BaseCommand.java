0633d5fce0d085b08faf59ab4aa29be041acb77c
Naoufal Alaa <alaanaoufal2000@gmail.com>
2022-12-26 11:24:44 +0100

    set Swagger | fix targetAggregateIdentifier

--- common-api/src/main/java/com/example/commonapi/commands/BaseCommand.java
+++ common-api/src/main/java/com/example/commonapi/commands/BaseCommand.java
@@ -1,9 +1,11 @@
 package com.example.commonapi.commands;
 
 import lombok.Getter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 public abstract class BaseCommand<T> {
     @Getter
+    @TargetAggregateIdentifier
     public T id;
 
     public BaseCommand(T id) {
